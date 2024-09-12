3269df5897e9e93546a4eb63dfb29213bc0f43b6
Anirudha <beleanirudha8@gmail.com>
2024-09-13 00:54:39 +0530

    pass props through components

--- 03-props-and-tailwindcss-in-react/src/App.jsx
+++ 03-props-and-tailwindcss-in-react/src/App.jsx
@@ -5,10 +5,15 @@ import Card from "./components/Card";
 function App() {
 	return (
 		<>
-			<h1 className="text-black bg-green-400 rounded-lg p-4 mb-4">
+			<h1 className="text-black bg-green-400 rounded-lg p-4 mb-4 cursor-pointer">
 				Tailwind CSS
 			</h1>
-			<Card />
+			<div className="flex gap-3 items-center justify-around">
+				<Card username="Ana" btnText="Click Me" />
+				<Card username="sana" btnText="Press Me" />
+				<Card username="dana" btnText="Visit Me" />
+				<Card username="Zyna" btnText="Touch Me" />
+			</div>
 		</>
 	);
 }
--- 03-props-and-tailwindcss-in-react/src/components/Card.jsx
+++ 03-props-and-tailwindcss-in-react/src/components/Card.jsx
@@ -1,4 +1,4 @@
-const Card = () => {
+const Card = ({ username, btnText }) => {
 	return (
 		<div className="relative h-[400px] w-[300px] rounded-md">
 			<img
@@ -8,13 +8,13 @@ const Card = () => {
 			/>
 			<div className="absolute inset-0 bg-gradient-to-t from-gray-900 to-transparent"></div>
 			<div className="absolute bottom-4 left-4 text-left">
-				<h1 className="text-lg font-semibold text-white">Delba</h1>
+				<h1 className="text-lg font-semibold text-white">{username}</h1>
 				<p className="mt-2 text-sm text-gray-300">
 					Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi,
 					debitis?
 				</p>
 				<button className="mt-2 inline-flex cursor-pointer items-center text-sm font-semibold text-white">
-					View Profile &rarr;
+					{btnText} &rarr;
 				</button>
 			</div>
 		</div>
