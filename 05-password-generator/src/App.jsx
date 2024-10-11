08ffdf5e8b14c4da24d47b331dceda1bd0466e5c
Anirudha <beleanirudha8@gmail.com>
2024-10-11 12:20:24 +0530

    delete unnecessary files

--- 05-password-generator/src/App.jsx
+++ 05-password-generator/src/App.jsx
@@ -1,4 +1,11 @@
+import { useState } from "react";
+
 const App = () => {
+	const [password, setPassword] = useState("");
+	const [length, setLength] = useState(8);
+	const [numberAllowed, setNumberAllowed] = useState(false);
+	const [characterAllowed, setCharacterAllowed] = useState(false);
+
 	return (
 		<>
 			<h1>Password Generator</h1>
